213600042379b55959338830a3375143c06dfd6d
Oswaldo Orellana <oswaldo.orellana.v@gmail.com>
2024-09-16 17:49:13 -0400

    agregando la parte de loading y error al redireccionar

--- src/components/pages/url-redirect-page/UrlRedirectPage.tsx
+++ src/components/pages/url-redirect-page/UrlRedirectPage.tsx
@@ -1,23 +1,46 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { UrlService } from "../../../services/UrlShortenerService";
+const urlService = new UrlService();
 
 export function UrlRedirectPage() {
-  const { code  } = useParams();
-  const urlService = new UrlService();
+  const { code } = useParams<{ code?: string }>();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
   
+
   useEffect(() => {
-    urlService.getUrlShortenerByCode(code!)
-      .then((response) => {
-        window.location.href = response.data.original_url;
-      })
-      .catch((error) => console.error(error));
-  })
+    if (code) {
+      urlService.getUrlShortenerByCode(code)
+        .then((response) => {
+          window.location.href = response.data.original_url;
+        })
+        .catch((error) => {
+          setLoading(false);
+          setError(`The code does not exist or an error occurred. ${error}`);
+        });
+    } else {
+      setLoading(false);
+      setError("Code not provided.");
+    }
+  }, [code]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
       <h1>URL Redirect Page</h1>
-      <p>Code from URL: {code}</p>
+      {error ? (
+        <div>
+          <p>{error}</p>
+          <button onClick={() => navigate('/')}>Go to Home Page</button>
+        </div>
+      ) : (
+        <p>Redirecting to the original URL...</p>
+      )}
     </div>
   );
 }
